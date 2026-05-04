76cf316dc7d050087a8a797841268f16ea0b4925
Gabriel Carmona <191816876+Gcarmnonapy7@users.noreply.github.com>
2026-05-04 00:21:40 -0300

    Adding files headers and implementations : matrix,model,vector,bindings and matrix.cpp

---
+++ cpp/src/bindings.cpp
@@ -0,0 +1,42 @@
+// #if defined(__has_include)
+// #  if __has_include(<pybind11/pybind11.h>)
+// #    include <pybind11/pybind11.h>
+// #  elif __has_include("pybind11/pybind11.h")
+// #    include "pybind11/pybind11.h"
+// #  else
+// #    error "pybind11 header not found. Adjust includePath to point to pybind11."
+// #  endif
+// #else
+// #  include <pybind11/pybind11.h>
+// #endif
+
+// #if defined(__has_include)
+// #  if __has_include(<pybind11/stl.h>)
+// #    include <pybind11/stl.h>
+// #  elif __has_include("pybind11/stl.h")
+// #    include "pybind11/stl.h"
+// #  else
+// #    error "pybind11 stl header not found. Adjust includePath to point to pybind11."
+// #  endif
+// #else
+// #  include <pybind11/stl.h>
+// #endif
+
+// #if defined(__has_include)
+// #  if __has_include(<pybind11/numpy.h>)
+// #    include <pybind11/numpy.h>
+// #  elif __has_include("pybind11/numpy.h")
+// #    include "pybind11/numpy.h"
+// #  else
+// #    error "pybind11 numpy header not found. Adjust includePath to point to pybind11."
+// #  endif
+// #else
+// #  include <pybind11/numpy.h>
+// #endif
+
+// #include "morphosml/vector.hpp"
+// #include "morphosml/matrix.hpp"
+// #include "morphosml/model.hpp"
+// #include "morphosml/metrics.hpp"
+
+// namespace py = pybind11;
